8cf7938851c25ddcee0c9e315b5b3cf8ade167a8
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2019-06-26 21:35:48 -0300

    Atualização

--- gerenciador/src/br/com/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/gerenciador/servlet/Banco.java
@@ -5,10 +5,10 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
-	
-	//Variavel estática  para simular um id do Banco 
+
+	// Variavel estática para simular um id do Banco
 	private static Integer chaveSequencial = 1;
-	//Variavel estática para criar uma lista de Empresas
+	// Variavel estática para criar uma lista de Empresas
 	private static List<Empresa> lista = new ArrayList<Empresa>();
 
 	static {
@@ -22,30 +22,39 @@ public class Banco {
 		empresa2.setNome("Microsoft");
 		lista.add(empresa2);
 	}
-	
-	//Método para adicionar uma empresa a lista
+
+	// Método para adicionar uma empresa a lista
 	public void adiciona(Empresa empresa) {
 		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
-	
-	//Método para retornar lista de empresas
+
+	// Método para retornar lista de empresas
 	public List<Empresa> getEmpresas() {
 		return Banco.lista;
 	}
-	
-	//Método para remover uma empresa
+
+	// Método para remover uma empresa
 	public void removeEmpresa(Integer id) {
-		
+
 		Iterator<Empresa> it = Banco.lista.iterator();
 		while (it.hasNext()) {
 			Empresa emp = it.next();
-			
+
 			if (emp.getId() == id) {
-				//removendo a empresa da lista 
+				// removendo a empresa da lista
 				it.remove();
 			}
-			
+
 		}
+	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if (empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
 	}
 }
